719009f02c88cf37e1a94f50d825bd3788516535
Danker4900 <dhorobets2023@itcollege.lviv.ua>
2026-05-30 15:45:00 +0300

    fix: socket io handler

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -4,22 +4,37 @@ import { Server as HttpServer } from "node:http";
 import { Server as SocketIOServer } from "socket.io";
 
 export const config = {
-	api: {
-		bodyParser: false,
-	},
+  api: {
+    bodyParser: false,
+  },
 };
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
-	if (!res.socket.server.io) {
-		const path = "/api/socket/io";
-		const httpServer: HttpServer = res.socket.server as any;
-		const io = new SocketIOServer(httpServer, {
-			path,
-			addTrailingSlash: false,
-		});
-		res.socket.server.io = io;
-	}
-	res.end();
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io";
+    const httpServer: HttpServer = res.socket.server as any;
+
+    const io = new SocketIOServer(httpServer, {
+      path,
+      addTrailingSlash: false,
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    io.on("connection", (socket) => {
+      console.log("🔌 socket connected:", socket.id);
+
+      socket.on("disconnect", () => {
+        console.log("❌ socket disconnected:", socket.id);
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+
+  res.end();
 };
 
 export default ioHandler;
